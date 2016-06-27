b0d910ee963af4d51e6265cc15331522954af66e
Erin <sylphofelectricity@gmail.com>
2016-06-27 14:52:05 -0500

    Add a BillboardSprite script

---
+++ The Genesis Project/Assets/Characters/Scripts/BillboardSprite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardSprite : MonoBehaviour {
+	void Update() {
+		if(m_CameraToFace.orthographic)
+			transform.LookAt(transform.position - m_CameraToFace.transform.forward, m_CameraToFace.transform.up);
+		else   
+			transform.LookAt(m_CameraToFace.transform.position, m_CameraToFace.transform.up);
+	}
+}
