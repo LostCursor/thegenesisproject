e2222ead4a51a3e3ddefeb3bd4e375538b0ec61f
Erin <sylphofelectricity@gmail.com>
2016-06-27 14:58:34 -0500

    Fix a bug in BillboardSprite.cs

--- The Genesis Project/Assets/Characters/Scripts/BillboardSprite.cs
+++ The Genesis Project/Assets/Characters/Scripts/BillboardSprite.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class BillboardSprite : MonoBehaviour {
+	public Camera m_CameraToFace;
+
 	void Update() {
 		if(m_CameraToFace.orthographic)
 			transform.LookAt(transform.position - m_CameraToFace.transform.forward, m_CameraToFace.transform.up);
