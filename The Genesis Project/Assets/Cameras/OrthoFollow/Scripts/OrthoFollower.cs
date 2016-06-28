5e9c40e648e0d09801e1108afaf1e174b0a724b9
Erin <sylphofelectricity@gmail.com>
2016-06-28 12:33:02 -0500

    OrthoFollow camera finished :smiley:

---
+++ The Genesis Project/Assets/Cameras/OrthoFollow/Scripts/OrthoFollower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrthoFollower : UnityStandardAssets.Cameras.AbstractTargetFollower
+{
+	public float dampTime = 0.3f;
+	public Vector3 cameraOffset = new Vector3(-8, 7, -8);
+
+	private Vector3 velocity = Vector3.zero;
+
+	protected override void FollowTarget(float dt)
+	{
+		Vector3 goalPosition = m_Target.position + cameraOffset;
+		transform.position = Vector3.SmoothDamp(transform.position, goalPosition, ref velocity, dampTime);
+	}
+}
