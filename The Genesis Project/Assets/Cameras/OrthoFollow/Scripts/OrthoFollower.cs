a06a5dc7511cd90e37e0564f0741b9c95fbf4361
gutza1 <peterushtoma@yahoo.com>
2016-06-29 15:24:19 -0400

    Added Camera flipping and compensated for it in movement code for John.
    See title.

--- The Genesis Project/Assets/Cameras/OrthoFollow/Scripts/OrthoFollower.cs
+++ The Genesis Project/Assets/Cameras/OrthoFollow/Scripts/OrthoFollower.cs
@@ -5,12 +5,42 @@ public class OrthoFollower : UnityStandardAssets.Cameras.AbstractTargetFollower
 {
 	public float dampTime = 0.3f;
 	public Vector3 cameraOffset = new Vector3(-8, 7, -8);
+    public bool flipped = false;
+    public bool flipping = false;
 
 	private Vector3 velocity = Vector3.zero;
+    private int rotationSpeed;
+    private int numRot = 0;
 
 	protected override void FollowTarget(float dt)
 	{
-		Vector3 goalPosition = m_Target.position + cameraOffset;
-		transform.position = Vector3.SmoothDamp(transform.position, goalPosition, ref velocity, dampTime);
+        Vector3 goalPosition = m_Target.position + cameraOffset;
+        if (flipping)
+        {
+            transform.RotateAround(m_Target.position, Vector3.up, rotationSpeed);
+            numRot++;
+            if (numRot == 30)
+            {
+                flipping = false;
+                flipped = !flipped;
+                cameraOffset.Set(-cameraOffset.x, cameraOffset.y, -cameraOffset.z);
+            }
+        }
+        else {
+            transform.position = Vector3.SmoothDamp(transform.position, goalPosition, ref velocity, dampTime);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            numRot = 0;
+            flipping = true;
+            rotationSpeed = 6;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            numRot = 0;
+            flipping = true;
+            rotationSpeed = -6;
+        }
 	}
 }
--- The Genesis Project/Assets/movement.cs
+++ The Genesis Project/Assets/movement.cs
@@ -5,12 +5,16 @@ public class movement : MonoBehaviour {
     
     // Create Variables.
     private Rigidbody John_Rigidbody;
+    private GameObject OrthoFollowerCamera;
+    private OrthoFollower followerScript;
     private float X_Speed;
     private float Z_Speed;
 
     // Get the rigidbody component from johnsprite and stoe it in John_Rigidbody variable.
     void Start() {
         John_Rigidbody = GetComponent<Rigidbody>();
+        OrthoFollowerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        followerScript = OrthoFollowerCamera.GetComponent<OrthoFollower>();
     }
 
 	// Update is called once per frame
@@ -22,6 +26,16 @@ public class movement : MonoBehaviour {
         // Vector3 changes the movement speed from an object by using 3 variables for each of the axises X(left/right), Y(up/down) and Z(forward/backward).
         // Because the camera is isometric sending just the X_speed or Z_speed variable would move john diagonally, So i send the X_Speed + the Z_Speed to move john left and right properly and the Z_Speed - the X_Speed to move john forwards and backwards properly.
         // The problem with this script is that it relies on the axis placement so when the camera changes position john's movement doesnt change with it. But its late and i can probably fix that tomorrow.
-        John_Rigidbody.velocity = new Vector3(X_Speed + Z_Speed, 0, Z_Speed - X_Speed);
+        if (!followerScript.flipping)
+        {
+            if (followerScript.flipped)
+            {
+                John_Rigidbody.velocity = new Vector3(-(X_Speed + Z_Speed), 0, -(Z_Speed - X_Speed));
+            }
+            else
+            {
+                John_Rigidbody.velocity = new Vector3(X_Speed + Z_Speed, 0, Z_Speed - X_Speed);
+            }
+        }
     }
 }
